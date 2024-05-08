94608f44077a5213f61c0dcb1edfdec3b58d883d
emonarob <121888915+emonarob@users.noreply.github.com>
2024-05-08 17:10:25 +0000

    dataytpes

---
+++ 01basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let acccountEmail ="emon007@gamil.com"
+var accounPassword ="12344565"
+accountCity = "dhaka"
+let accountState;
+//accountId = 2//not allowed
+
+acccountEmail = "lala@gmail.com"
+accountCity = "chiagong"
+accounPassword = "3413412341"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scopes and funcional scope
+*/
+console.table([acccountEmail,accounPassword,accountCity,accountId,accountState])
+
---
+++ 01basics/02_dataypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using noe js not browser
+
+console.log(3+3)//code readability should be high
+
+console.log("emon")
+
+let name ="Emon"
+let age = 22
+let isLoggedIn = true
+
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => True or false
+//null  => standalone value
+//undefined => 
+//symbol =>unique
+
+
+//object 
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
