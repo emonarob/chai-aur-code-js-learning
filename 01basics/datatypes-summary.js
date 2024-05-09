3c1ae625e40bc686bbe71411835a3f921d79070c
emonarob <121888915+emonarob@users.noreply.github.com>
2024-05-09 10:37:41 +0000

    datatype-summary

---
+++ 01basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+//primitive
+
+// 7 types : string, number, boolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+
+
+//console.log(id === anotherId);
+
+const BigNumber = 12312312313131313123131n
+
+// reference (non primitive)
+
+// arrays, objects, functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj ={
+    name: "Emon",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof BigNumber);
