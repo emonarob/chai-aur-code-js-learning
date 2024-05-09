026340e04ee1a30fdf24ed40bc10fe7407cc7c64
emonarob <121888915+emonarob@users.noreply.github.com>
2024-05-09 19:17:19 +0000

    numbers_and_math

---
+++ 01basics/06_nums_and_math.js
@@ -0,0 +1,32 @@
+const score = 400
+
+const balance = new Number(100)
+
+
+const otherNumber = 23.3434
+//console.log (otherNumber.toPrecision(4));
+
+const hundreds = 1000000000000
+
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++ maths +++++++++//
+console.log(Math.abs(-4));
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,9,3,4,6,7,7));
+console.log(Math.max(4,9,3,4,6,7,7));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
+
+
