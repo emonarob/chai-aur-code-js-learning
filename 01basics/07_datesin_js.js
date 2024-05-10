25354f9d234c00aca81696e40f5d4a08e26c9914
emonarob <121888915+emonarob@users.noreply.github.com>
2024-05-10 18:52:16 +0000

    date and arrays

---
+++ 01basics/07_datesin_js.js
@@ -0,0 +1,13 @@
+// dates
+
+let mydate = new Date();
+//
+
+//let myCreatedDate = new Date(2079, 3, 24)
+//let myCreatedDate = new Date(2079, 3, 24, 5, 4)
+let myCreatedDate = new Date("2033-01-23")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+// arrays
+
+const myArr = [0, 1, 2, 3, 4]
+const myHeros = ["ironman", "spiderman","captainAmarica"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5, 4)
+
+console.log(myArr[3]);
+
+// Array method 
+
+// myArr.push(8)
+// myArr.push(2)
+// myArr.pop()
+// myArr.unshift(55)
+// myArr.shift()
+// console.log(myArr.includes(4));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myn2);
